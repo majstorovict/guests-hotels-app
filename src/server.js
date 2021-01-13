4083ad17cb66fcbb8a7aff54d41599e6aca8693d
majstorovict <tanja.majstorovic@dev.telnet.rs>
2021-01-13 14:31:29 +0100

    new

--- src/server.js
+++ src/server.js
@@ -1,18 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/guest-hotels'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/guest-hotels'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
-  
+const path = require('path');
+app.use(express.static(__dirname + '/dist/guest-hotels'));
+app.get('/*', function(req, res) {
+res.sendFile(path.join(__dirname + '/dist/guest-hotels/index.html'));
+})
+app.listen(process.env.PORT || 8080);
