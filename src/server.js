2824c95145869936f8bcfcfb0c1516ea2ff1f768
majstorovict <tanja.majstorovic@dev.telnet.rs>
2021-01-14 11:00:44 +0100

    update

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/guest-hotels'));
+app.use(express.static(__dirname + '/dist/guest-hotels'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/guest-hotels'}),
+    res.sendFile('index.html', {root: '/dist/guest-hotels'}),
 );
 // Start the app by listening on the default Heroku port
 //set the port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Example app listening at http://localhost:`)
+});
