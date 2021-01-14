64b2f9f26e17147a50294ce5013874d6b3aed253
majstorovict <tanja.majstorovic@dev.telnet.rs>
2021-01-14 10:55:49 +0100

    new u

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
-let app = express();
-
+const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/guest-hotels'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/guest-hotels/index.html'));
-});
-
+app.use(express.static(__dirname + './dist/guest-hotels'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/guest-hotels'}),
+);
 // Start the app by listening on the default Heroku port
+//set the port
 app.listen(process.env.PORT || 8080);
